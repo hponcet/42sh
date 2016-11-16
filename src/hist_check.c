2a0de445d06e0c91cf33932cd5f97b38720c3821
Hugues PONCET <hponcet@e3r9p9.42.fr>
2016-11-16 20:32:13 +0100

    Tricks dans le check des doublons pour checker le dernier historique tape

--- src/hist_check.c
+++ src/hist_check.c
@@ -6,7 +6,7 @@
 /*   By: hponcet <hponcet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/15 04:37:52 by hponcet           #+#    #+#             */
-/*   Updated: 2016/11/15 04:44:19 by hponcet          ###   ########.fr       */
+/*   Updated: 2016/11/16 20:17:24 by hponcet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int			hist_checkdouble(t_shell *shell)
 {
+	t_hist	*thist;
 	char	*tmp;
 	char	*tmp2;
 	int		i;
@@ -21,11 +22,14 @@ int			hist_checkdouble(t_shell *shell)
 	i = 0;
 	if (!(shell->hist))
 		return (i);
+	thist = shell->hist;
 	tmp = input_to_char(shell->input);
-	tmp2 = input_to_char(shell->hist->input);
+	while (thist->next)
+		thist = thist->next;
+	tmp2 = input_to_char(thist->input);
 	if (ft_strcmp(tmp, tmp2) == 0)
 		i = 1;
 	ft_strdel(&tmp);
 	ft_strdel(&tmp2);
-	return(i);
+	return (i);
 }
