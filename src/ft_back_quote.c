679ebfda70d86e20b14855037f3a81fe3217f4e9
Mr_Robot <mrrobot@mr-robots-macbook-pro.home>
2016-11-20 11:57:20 +0100

    trop de questio pour flav

--- src/ft_back_quote.c
+++ src/ft_back_quote.c
@@ -13,19 +13,24 @@
 #include "sh.h"
 
 //quote dans backquote
-/*
-static void	ft_treat_back_quote(char *cmd, t_input **input, int *fd)
+
+static void	ft_treat_back_quote(char *str, t_input **input, int *fd)
 {
-	
+	ft_putstr(cmd); // debug
+	// check si redirection ou bulttin
+	char	**cmd
+	char	*path;
+
+	//ouvrir fd, dup exec la commande dedans 
 }
-*/
+
 
 static size_t	ft_list_size_index(t_input *start, t_input *end)
 {
 	size_t size;
 
 	size = 0;
-	while (start != NULL && start != end->next)
+	while (start != end->next)
 	{
 		start = start->next;
 		size++;
@@ -38,13 +43,13 @@ static char		*ft_lst_to_str_index(t_input **start, t_input *end)
 	char	*cmd;
 	int		i;
 
+	i = 0;
 	if ((*start)->c == '`')
 	{
 		*start = (*start)->next;
 		return (NULL);
 	}
 	cmd = ft_strnew(ft_list_size_index(*start, end));
-	i = 0;
 	while (*start != end)
 	{
 		cmd[i] = (*start)->c;
@@ -61,8 +66,6 @@ void	ft_back_quote(t_input **input, int *fd)
 	t_input	*tmp;
 	char	*cmd;
 
-	int i;
-	i =fd[1];
 	ptr = *input;
 	while (ptr != NULL)
 	{
@@ -73,9 +76,7 @@ void	ft_back_quote(t_input **input, int *fd)
 				tmp = tmp->next;
 			cmd = ft_lst_to_str_index(&ptr->next, tmp->prev);
 			if (cmd != NULL)
-				ft_putstr(cmd);
-		//	if (cmd != NULL)
-		//		ft_treat_back_quote(cmd, input, fd);
+				ft_treat_back_quote(cmd, input, fd);
 			ptr = tmp->next;
 		}
 		else
